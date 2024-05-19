04058497821a4980c1cdcc1916d18b9f8851c369
truonglt <truongltse05868@fpt.edu.vn>
2024-05-20 03:06:58 +0700

    add userProfile

--- SWP391_KRS_Group6/src/java/controller/UserController.java
+++ SWP391_KRS_Group6/src/java/controller/UserController.java
@@ -5,6 +5,9 @@
 
 package controller;
 
+import dao.UserDAO;
+import entity.User;
+import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -12,12 +15,17 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  *
  * @author HuyPC
  */
-@WebServlet(name="UserController", urlPatterns={"/UserController"})
+//@WebServlet(name="UserController", urlPatterns={"/UserController"})
 public class UserController extends HttpServlet {
    
     /** 
@@ -27,21 +35,31 @@ public class UserController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    private static final Logger logger = Logger.getLogger(UserListController.class.getName());
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UserController</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UserController at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+
+        try {
+            // Lấy userId từ request parameter
+            int userId = Integer.parseInt(request.getParameter("id"));
+                        // Initialize UserDAO
+            UserDAO userDAO = new UserDAO();
+            // Fetch list of users from the database
+            User users = userDAO.getUserById(userId);
+
+            // Set the list of users as a request attribute
+            request.setAttribute("users", users);
+
+            // Forward the request to the JSP page
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/userProfile.jsp");
+            dispatcher.forward(request, response);
+        }catch (NumberFormatException e) {
+            logger.log(Level.SEVERE, "Invalid user ID format", e);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid user ID format");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error processing user request", e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred while processing your request.");
+    }
     } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
