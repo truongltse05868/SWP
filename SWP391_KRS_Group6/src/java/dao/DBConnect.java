cca9dafae01fa8087a83d6b62b7c54804d3abcbc
truonglt <truongltse05868@fpt.edu.vn>
2024-05-30 01:04:21 +0700

    verify

--- SWP391_KRS_Group6/src/java/dao/DBConnect.java
+++ SWP391_KRS_Group6/src/java/dao/DBConnect.java
@@ -22,12 +22,10 @@ public class DBConnect {
             //connection của Truonglt
             String user = "root";
 
-          String pass = "Mysql-suck"; //duc
+//          String pass = "Mysql-suck"; //duc
 //            String pass = "Simon041098@"; //mạc bục
 //            String pass = "Simon@0410"; //máy công ty
-
-
-//            String pass = "Simon0410@"; //PC
+            String pass = "Simon0410@"; //PC
 
             String url = "jdbc:mysql://127.0.0.1:3306/KRS_SWR?zeroDateTimeBehavior=CONVERT_TO_NULL";
             Class.forName("com.mysql.cj.jdbc.Driver");
